33fdc1f3fd82dce0a245cc4eedcb6b79e591c9eb
swar186j <74865434+swar186j@users.noreply.github.com>
2021-07-29 22:18:17 +0530

    Update and rename app.py to Module 1/app.py

--- Module 1/app.py
+++ Module 1/app.py
@@ -1,19 +1,17 @@
-#create virtualenv env
-#cd to directory if you wish
-#env/Scripts/activate
-#templates are generally the blueprints of the html code that you're going to use in webpage and will get rendered. 
+#Main App
 from flask import Flask, render_template, url_for
 
 app= Flask(__name__)
 
-# making list of pokemons
+# making list of contents covered
 Learning_flow =["Installation", "Application Structure", "Templates", "Web Forms", "Database", "Email", "Testing", "Performance", "Deployment", "Resources"]
 
 @app.route('/')
 def index():
-    return render_template('index.html', author='Swarada',Learning_flow= Learning_flow) #created first template
+    #created first template
+    return render_template('index.html', author='Swarada',Learning_flow= Learning_flow) 
 
-
+#in case page doesn't exists, show the errors
 @app.errorhandler(404)
 def page_not_found(e):
  return render_template('404.html'), 404
@@ -23,9 +21,6 @@ def internal_server_error(e):
  return render_template('500.html'), 500
 
 
-
-
- 
 if __name__=="__main__":
     
     app.run(debug=True)
